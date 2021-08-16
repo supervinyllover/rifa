c9010e6030b45646fea2a3147122ff1c6f6f58a1
Teles <jose.maciel@vivadecora.com.br>
2021-08-15 22:57:34 -0300

    Atualiza numero do WhatsApp

--- components/Raffle.vue
+++ components/Raffle.vue
@@ -7,11 +7,12 @@
       Selecione um número para enviar uma mensagem direto para o Andrew via WhatsApp
     </p>
     <ul class="raffle">
-
       <li
         :class="raffleNumber.vendido ? 'raffle__item--inactive' : 'raffle__item'"
         v-for="(raffleNumber, index) in raffle" :key="index">
-        {{raffleNumber.numero}}
+        <a class="raffle__anchor" :href="`https://wa.me/5511971676491/?text=Oi,%20Andrew.%20Quero%20participar%20da%20rifa%20com%20o%20n%C3%BAmero%20${raffleNumber.numero}`">
+          {{raffleNumber.numero}}
+        </a>
       </li>
     </ul>
   </section>
@@ -65,5 +66,12 @@ export default Vue.extend({
 .raffle__item--inactive {
   opacity: .1;
   cursor: initial;
+}
+.raffle__anchor {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  color: inherit;
+  text-decoration: none;
 }
 </style>
